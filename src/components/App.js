b67b6dc8bf0e597208f6ca3aee3f070d59c46eae
Dzmitry <dzmitriy-melnikov@yandex.ru>
2018-04-21 11:37:52 +0300

    add user form

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import ArticleList from './ArticleList';
 import ArticlesChart from './ArticlesChart';
+import UserForm from "./userForm";
 
 export class App extends Component{
     static propTypes = {
@@ -10,7 +11,7 @@ export class App extends Component{
     render() {
         return(
             <div>
-                Name: <input type='text' />
+                <UserForm/>
                 <ArticleList articles={this.props.articles}/>
                 <ArticlesChart articles={this.props.articles}/>
             </div>
---
+++ src/components/userForm.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class UserForm extends Component{
+  state = {
+    userName: ''
+  };
+
+  handleChange = ev => {
+    this.setState({
+      userName: ev.target.value
+    });
+    console.log(this.state.userName);
+  };
+
+  render() {
+    return(
+        <div>
+          <input type="text" value={this.state.userName} onChange={this.handleChange}/>
+        </div>
+      )
+
+  }
+
+}
